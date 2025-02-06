0e6d80187303366dcffc4ad419b09c99371b0890
aaafeig <geroy.ozhog08@mail.ru>
2025-02-06 19:31:35 +0300

    fixed task 4

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         var friend = 19;
-        friend = (friend + 2) / 7;
+        System.out.println(friend);
+        friend += 2;
+        System.out.println(friend);
+        friend /= 7;
         System.out.println(friend);
     }
 }
