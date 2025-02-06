bd17953325f3faa2372c84c7cc88e6dc76c87da7
aaafeig <geroy.ozhog08@mail.ru>
2025-02-06 19:25:29 +0300

    created a new variable "friend"

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        var dog = 8.0 + 4 - 3.5;
-        var cat = 3.6 + 4 - 3.5;
-        var paper = 763789 + 4 - 3.5;
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
+        var friend = 19;
+        friend = (friend + 2) / 7;
+        System.out.println(friend);
     }
 }
