f5a101124ed958ad5d2b6c8d7d5cc018d44196ed
aaafeig <geroy.ozhog08@mail.ru>
2025-02-06 19:49:21 +0300

    task 8.2

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@ public class Main {
     public static void main(String[] args) {
         var hoursOfWork = 640;
         var hoursOnWork = 8;
-        var people = hoursOfWork / hoursOnWork;
-        System.out.println("Всего работников в компании - " + people + " человек");
+        var peopleFirstCompany = hoursOfWork / hoursOnWork;
+        var peopleSecondCompany = peopleFirstCompany + 94;
+        hoursOfWork = peopleSecondCompany * hoursOnWork;
+        System.out.println("Если в компании работает " + peopleSecondCompany + " человек, то всего " + hoursOfWork + " часов работы может быть поделено между сотрудниками");
     }
 }
