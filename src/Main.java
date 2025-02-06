b7c7a3af7b4c21e03efc4a177d88338a325bf106
aaafeig <geroy.ozhog08@mail.ru>
2025-02-06 19:39:24 +0300

    task 7

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args) {
         var boxerWeight1 = 78.2;
         var boxerWeight2 = 82.7;
-        System.out.println(boxerWeight1 + boxerWeight2);
-        System.out.println(boxerWeight2 - boxerWeight1);
+        System.out.println(boxerWeight2 % boxerWeight1);
     }
 }
