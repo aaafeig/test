b49fcdee1a4a45815446e085f0fc8b32fa434ecd
aaafeig <geroy.ozhog08@mail.ru>
2025-02-06 19:22:30 +0300

    created a new values for variable

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        var dog = 8.0 + 4;
-        var cat = 3.6 + 4;
-        var paper = 763789 + 4;
+        var dog = 8.0 + 4 - 3.5;
+        var cat = 3.6 + 4 - 3.5;
+        var paper = 763789 + 4 - 3.5;
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
