5df9836d30d2c14ab2b18221d67cbf93d856b2af
aaafeig <geroy.ozhog08@mail.ru>
2025-02-10 19:29:14 +0300

    task 2

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        byte apples = 8;
-        System.out.println("Значение переменной apples с типом byte равно "+ apples);
-        short potatoes = 20000;
-        System.out.println("Значение переменной potatoes с типом short равно "+ potatoes);
-        int moneys = 1000000;
-        System.out.println("Значение переменной moneys с типом int равно "+ moneys);
-        long people = 8000000000L;
-        System.out.println("Значение переменной people с типом long равно "+ people);
-        float something = 1.5f;
-        System.out.println("Значение переменной something с типом float равно "+ something);
-        double piNumber = 3.1415926535f;
-        System.out.println("Значение переменной piNumber с типом double равно "+ piNumber);
-
+        float something = 27.12f;
+        long things = 987678965549L;
+        short apples = 2786;
+        short numberPos = 569;
+        short numbersNeg = -159;
+        short cars = 27897;
+        byte students = 67;
     }
 }
