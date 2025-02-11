6f7c2b5b671fc3496375b9836e76dff21c829121
aaafeig <geroy.ozhog08@mail.ru>
2025-02-11 20:29:55 +0300

    task 7 for average value

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-    float weightLoss = 0.5f;
+    float weightLoss500 = 0.5f;
+    float weightLoss250 = 0.250f;
     byte weight = 7;
-    System.out.println("Спортсмену потребуется " + weight/weightLoss + " дней");
+    System.out.println("Спортсмену потребуется " + ((weight / weightLoss500) + (weight / weightLoss250)) / 2 + " дней в среднем");
     }
 }
