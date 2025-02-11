fe101629468b77ba3732f7e43ac5680f2523815e
aaafeig <geroy.ozhog08@mail.ru>
2025-02-11 20:42:20 +0300

    task 8

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-    float weightLoss500 = 0.5f;
-    float weightLoss250 = 0.250f;
-    byte weight = 7;
-    System.out.println("Спортсмену потребуется " + ((weight / weightLoss500) + (weight / weightLoss250)) / 2 + " дней в среднем");
+        float percent = 0.1f;
+        int salaryMasha = 67760;
+    System.out.println("Маша теперь получает " + (salaryMasha * percent + salaryMasha) + " рублей. Годовой доход вырос на " + ((salaryMasha * percent + salaryMasha) - salaryMasha) * 12 + " рублей");
+    int salaryDen = 83690;
+        System.out.println("Денис теперь получает " + (salaryDen * percent + salaryDen) + " рублей. Годовой доход вырос на " + ((salaryDen * percent + salaryDen) - salaryDen) * 12 + " рублей");
+    int salaryCris = 76230;
+        System.out.println("Кристина теперь получает " + (salaryCris * percent + salaryCris) + " рублей. Годовой доход вырос на " + ((salaryCris * percent + salaryCris) - salaryCris) * 12 + " рублей");
+
     }
 }
