ab121d1df5b1e06a6a049a7b5fa777384ecbe662
aaafeig <geroy.ozhog08@mail.ru>
2025-02-11 20:26:38 +0300

    task 7 for 500g

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-    float weightLoss = 0.250f;
+    float weightLoss = 0.5f;
     byte weight = 7;
     System.out.println("Спортсмену потребуется " + weight/weightLoss + " дней");
     }
