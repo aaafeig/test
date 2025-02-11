10dff4c8498312d731855028332c7b1ae05f1e4f
aaafeig <geroy.ozhog08@mail.ru>
2025-02-11 20:25:10 +0300

    task 7 for 250g

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-    float weightBananas = 0.08f * 5;
-    float weightMilks = 0.105f * 2;
-    float weightIceCream = 0.100f * 2;
-    float weightEggs = 0.070f * 4;
-    System.out.println("Завтрак весит - " + (weightBananas + weightMilks + weightIceCream + weightEggs) + " килограмм");
+    float weightLoss = 0.250f;
+    byte weight = 7;
+    System.out.println("Спортсмену потребуется " + weight/weightLoss + " дней");
     }
 }
