08f7e79bd5ec02101e805125c9e5282c5e0d21c1
aaafeig <geroy.ozhog08@mail.ru>
2025-02-13 18:03:55 +0300

    homework_3 fix

--- src/Main.java
+++ src/Main.java
@@ -25,12 +25,12 @@ public class Main {
     }
 
     //4
-    byte age2 = 18;
-    if (2 < age2 && age2 < 6){
+    byte age2 = 17;
+    if (2 <= age2 && age2 <= 6){
         System.out.println("нужно ходить в детский сад");
-    } else if (7 < age2 && age2 < 17) {
+    } else if (7 <= age2 && age2 <= 17) {
         System.out.println("нужно ходить в школу");
-    } else if (18 < age2 && age2 < 24) {
+    } else if (18 <= age2 && age2 <= 24) {
         System.out.println("нужно ходить в универ");
     }else {
         System.out.println("нужно работать");
@@ -39,21 +39,22 @@ public class Main {
     //5
     byte ageChile = 14;
     if (ageChile < 5) {
-        System.out.println("Вы не может кататься на аттракционе");
-    }else if (5 < ageChile && ageChile < 14) {
+        System.out.println("Ты не может кататься на аттракционе");
+    }else if (5 <= ageChile && ageChile <= 14) {
         System.out.println("Ты можешь кататься, но только со взрослым");
     }else {
         System.out.println("Ты можешь кататься без сопровождения взрослого");
     }
 
     //6
+    byte allPositions = 102;
     byte  seatingPositions = 60;
     byte occupiedPositions = 55;
     if (occupiedPositions < seatingPositions) {
         System.out.println("Есть сидячие места");
     }else if (occupiedPositions >= seatingPositions) {
         System.out.println("Нет сидячих мест, придется стоять");
-    }else {
+    }else if (occupiedPositions == allPositions) {
         System.out.println("Вагон забит");
     }
 
@@ -64,7 +65,7 @@ public class Main {
     if (one > two && one > three) {
         System.out.println("Число " + one + " большее");
     }else if (two > one && two > three) {
-        System.out.println("Число" + one + " большее");
+        System.out.println("Число" + two + " большее");
     } else{
         System.out.println("Число " + three + " большее");
     }
